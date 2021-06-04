5364efe90ccb925995484cca51b155eac8ac8afc
Maxim <ififorgetyou@gmail.com>
2021-06-04 21:24:23 +0300

    prep: hw 3

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const $arenas = document.querySelector(".arenas");
+const $randomButton = document.querySelector(".button");
 
 let player1 = {
+  player: 1,
   name: "SCORPION",
-  hp: 100,
+  hp: 90,
   img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
   weapon: ["knife"],
   attack: function () {
@@ -11,6 +13,7 @@ let player1 = {
 };
 
 let player2 = {
+  player: 2,
   name: "SUB-ZERO",
   hp: 100,
   img: "http://reactmarathon-api.herokuapp.com/assets/subzero.gif",
@@ -20,27 +23,29 @@ let player2 = {
   },
 };
 
-function createPlayer(player, obj) {
-  const $tempPlayer = document.createElement("div");
-  $tempPlayer.classList.add(player);
+function createElement(tag, className) {
+  const $tag = document.createElement(tag);
+  if (className) {
+    $tag.classList.add(className);
+  }
+  return $tag;
+}
 
-  const $tempProgressBar = document.createElement("div");
-  $tempProgressBar.classList.add("progressbar");
+function createPlayer(playerObj) {
+  const $tempPlayer = createElement("div", `player${playerObj["player"]}`);
+  const $tempProgressBar = createElement("div", "progressbar");
+  const $tempCharacter = createElement("div", "character");
+  const $tempLife = createElement("div", "life");
+  const $tempName = createElement("div", "name");
 
-  const $tempCharacter = document.createElement("character");
-  $tempCharacter.classList.add("character");
+  const $tempImg = createElement("img");
 
-  const $tempLife = document.createElement("life");
-  $tempLife.classList.add("life");
-  $tempLife.style.width = `${obj["hp"]}%`;
-  $tempLife.innerText = obj["hp"];
+  $tempLife.style.width = `${playerObj["hp"]}%`;
+  $tempLife.innerText = playerObj["hp"];
 
-  const $tempName = document.createElement("name");
-  $tempName.classList.add("name");
-  $tempName.innerText = obj["name"];
+  $tempName.innerText = playerObj["name"];
 
-  const $tempImg = document.createElement("img");
-  $tempImg.src = obj["img"];
+  $tempImg.src = playerObj["img"];
 
   $tempProgressBar.appendChild($tempLife);
   $tempProgressBar.appendChild($tempName);
@@ -48,8 +53,32 @@ function createPlayer(player, obj) {
   $tempPlayer.appendChild($tempProgressBar);
   $tempPlayer.appendChild($tempCharacter);
 
-  $arenas.appendChild($tempPlayer);
+  return $tempPlayer;
 }
 
-createPlayer("player1", player1);
-createPlayer("player2", player2);
+function changeHP(player) {
+  const $playerLife = document.querySelector(
+    ".player" + player["player"] + " .life"
+  );
+  player.hp -= 20;
+  $playerLife.style.width = `${player["hp"]}%`;
+
+  if (player["hp"] < 0) {
+    $arenas.appendChild(playerLose(player["name"]));
+  }
+}
+
+function playerLose(name) {
+  const $loseTitle = createElement("div", "loseTitle");
+  $loseTitle.innerText = `${name} lose`;
+  return $loseTitle;
+}
+
+$randomButton.addEventListener("click", function () {
+  console.log("####: click");
+  changeHP(player1);
+  changeHP(player2);
+});
+
+$arenas.appendChild(createPlayer(player1));
+$arenas.appendChild(createPlayer(player2));
